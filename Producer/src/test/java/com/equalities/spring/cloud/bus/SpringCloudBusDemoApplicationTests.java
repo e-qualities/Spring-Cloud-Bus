11895b611f7dbb7feab660dbdee0f2a784dcfd08
FWinkler79 <52044081+FWinkler79@users.noreply.github.com>
2020-07-01 17:45:37 +0200

    Initial Project Setup.

---
+++ Producer/src/test/java/com/equalities/spring/cloud/bus/SpringCloudBusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equalities.spring.cloud.bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudBusDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
